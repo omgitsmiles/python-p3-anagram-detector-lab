95d570d4ca7f607a647ab229f1556f2b7975ced7
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-04-25 17:18:06 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+# class Anagram:
+#     def __init__(self, word):
+#         self.word = word
+
+#     def match(self, word):
+#         word_len = [w for w in word if len(w) == len(self.word)]
+
+class Anagram:
+    def __init__(self, word):
+        
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == self.sorted_word and w.lower() != self.word]
